aacddbc1b85656bde9a2d6664ddca5384b276149
Carlos Ayala <49286868+SrMai@users.noreply.github.com>
2022-09-06 13:06:08 -0500

    v1.0
    Se añadieron nuevas características lógicas.
    Se añadieron 2 nuevas operaciones.

--- calculadora.py
+++ calculadora.py
@@ -12,46 +12,54 @@ def menu():
     print("2.-Resta")
     print("3.-Multiplicacion")
     print("4.-Division")
+    print("5.-Potencia")
+    print("6.-Porcentaje")
     print("-------------------------------")
     print("Escoja una opción.")
+    try:
+        operacion = int(input("Elije una opcion: "))
+        if operacion >= 7: #Colocar numero de opcion que no tengas
+            os.system("cls")
+            print ("Error: Por favor de escribir solo numeros del menú.")
+            exit()
+        numero1 = float(input("Numero 1: "))
+        numero2 = float(input("Numero 2: "))
+    except ValueError:
+        print ("Error: Por favor de escribir solo numeros")
     
-    operacion = input("")
-    os.system("clear")
-    print("Escribe el número 1.")
-    numero1 = input("")
-    print("Escribe el número 2.")
-    numero2 = input("")
-    operaciones(operacion, int(numero1), int(numero2))
+    operaciones(operacion, float(numero1), float(numero2))
 
 def operaciones(operacion, numero1, numero2):
-    if operacion == '1':#Suma
+    if operacion == 1:#Suma
         print ("Suma: ")
         print(str(numero1)+"+"+str(numero2))
         print("=")
         print (numero1+numero2)
-    elif operacion == '2':#Resta
+    elif operacion == 2:#Resta
         print ("Resta: ")
         print(str(numero1)+"-"+str(numero2))
         print("=")
         print (numero1-numero2)
-    elif operacion == '3':#Multiplicación
+    elif operacion == 3:#Multiplicación
         print ("Multiplicación: ")
         print(str(numero1)+"*"+str(numero2))
         print("=")
         print (numero1*numero2)
-    elif operacion == '4':#División
+    elif operacion == 4:#División
         print ("División: ")
         print(str(numero1)+"/"+str(numero2))
         print("=")
         print (numero1/numero2)
+    elif operacion == 5:#Potencia
+        print ("Potencia: ")
+        print(str(numero1)+"**"+str(numero2))
+        print("=")
+        print (numero1**numero2)
+    elif operacion == 6:#Porcentaje
+        print ("Porcentaje: ")
+        print(str(numero1)+"*"+str(numero2)+"/100")
+        print("=")
+        print (numero1*numero2/100)
     
+
 menu()
-#if '*' in opcion:
-#    print ("Multiplicacion")
-#elif '+' in opcion:
-#    print ("Suma")
-#elif '-' in opcion:
-#    print ("Resta")
-#elif '/' in opcion:
-#    print ("Division")
-    
